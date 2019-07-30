5f0c900316b39f63a5f5162dc0bfd007fa2c4003
jennystarr <jennifermagana22@gmail.com>
2019-07-30 13:27:44 -0400

    school

--- schoolsamp.py
+++ schoolsamp.py
@@ -3,5 +3,11 @@ list_of_scores = school_scores.get_all()
 
 print(list_of_scores)
 print(list_of_scores[0])
+
+#gets out state name and year
 for row in list_of_scores:
     print(row["State"]["Name"], row["Year"])
+
+#gets total test takers
+for row in list_of_scores:
+    print(row["Total"]["Test-takers"])
