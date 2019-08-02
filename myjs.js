847e7b52b415aa7a7833ac43a9346fa82997a6cb
jennystarr <jennifermagana22@gmail.com>
2019-08-02 15:28:25 -0400

    website

--- myjs.js
+++ myjs.js
@@ -1,2 +1,41 @@
 //add an alert box for page to test connection
-alert("welcome to my java script")
+function readMore(){
+  var x = document.getElementById("hiddenvid");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function dance2(){
+    var x = document.getElementById("hiddenvid2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function dance3(){
+  var x = document.getElementById("hiddenvid3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function dance4(){
+  var x = document.getElementById("hiddenvid4");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function dance5(){
+  var x = document.getElementById("hiddenvid5");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
